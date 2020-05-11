2a5b2de87fbd065fb47c269824f7432721743436
kevinsapp <sapp.kevin@gmail.com>
2020-05-11 22:43:11 +0300

    Removed redundant test

--- cmd/migration_test.go
+++ cmd/migration_test.go
@@ -1,54 +1,11 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/kevinsapp/monarch/pkg/sql"
 	"github.com/spf13/cobra"
 )
-
-// Unit test createMigration()
-func TestCreateMigration(t *testing.T) {
-	// Create a migrations directory.
-	var cmd = &cobra.Command{}
-	var args []string
-	mkdirMigrations(cmd, args)
-	defer os.RemoveAll("migrations") // Do cleanup
-
-	// Set timestamp and table data.
-	timestamp := time.Now().UnixNano()
-	td := table{}
-	td.Name = "users"
-
-	// Create a migration file.
-	fn := fmt.Sprintf("migrations/%d_create_table_%s_up.sql", timestamp, td.Name)
-	f, err := createMigration(fn, sql.CreateTableTmpl, td)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Check that the migration file has the expected name.
-	exp := fn
-	act := f.Name()
-	if exp != act {
-		t.Errorf("want %s; got %s", exp, act)
-	}
-
-	// Check that the migration file contains the expected content.
-	exp = testCreateTableSQL
-	act, err = sql.FileAsString(fn)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if exp != act {
-		t.Errorf("want %s; got %s", exp, act)
-	}
-
-}
 
 // Unit test mkdirMigrations()
 func TestMkdirMigrations(t *testing.T) {
