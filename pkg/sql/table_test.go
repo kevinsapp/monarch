0e61eee99423e681bc9a88698e50bef4dbb37970
kevinsapp <sapp.kevin@gmail.com>
2020-05-15 13:53:00 +0300

    Added tests

--- pkg/sql/table_test.go
+++ pkg/sql/table_test.go
@@ -79,3 +79,104 @@ func TestTableSetNewName(t *testing.T) {
 		}
 	}
 }
+
+// Unit test Table.Columns()
+func TestTableColumns(t *testing.T) {
+	expCols := []Column{
+		{name: "id", colType: "uuid"},
+		{name: "givenName", colType: "varchar"},
+		{name: "familyName", colType: "character varying"},
+		{name: "created_at", colType: "timestamp(6) without time zone"},
+		{name: "updated_at", colType: "timestamp(6) without time zone"},
+	}
+
+	// Assign expCols to Table.columns
+	tbl := Table{name: "users"}
+	tbl.columns = expCols
+
+	actCols := tbl.Columns() // Run Table.Columns()
+
+	for i, v := range expCols {
+		// Compare column names
+		exp := v.name
+		act := actCols[i].name
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+
+		// Compare column types
+		exp = v.colType
+		act = actCols[i].colType
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+	}
+}
+
+// Unit test Table.SetColumns()
+func TestTableSetColumns(t *testing.T) {
+	expCols := []Column{
+		{name: "id", colType: "uuid"},
+		{name: "givenName", colType: "varchar"},
+		{name: "familyName", colType: "character varying"},
+		{name: "created_at", colType: "timestamp(6) without time zone"},
+		{name: "updated_at", colType: "timestamp(6) without time zone"},
+	}
+
+	// Assign expCols to Table.columns
+	tbl := Table{name: "users"}
+	tbl.SetColumns(expCols) // Run Table.SetColumns()
+
+	actCols := tbl.columns
+
+	for i, v := range expCols {
+		// Compare column names
+		exp := v.name
+		act := actCols[i].name
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+
+		// Compare column types
+		exp = v.colType
+		act = actCols[i].colType
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+	}
+}
+
+// Unit test Table.AddColumn()
+func TestTableAddColumn(t *testing.T) {
+	expCols := []Column{
+		{name: "id", colType: "uuid"},
+		{name: "givenName", colType: "varchar"},
+		{name: "familyName", colType: "character varying"},
+		{name: "created_at", colType: "timestamp(6) without time zone"},
+		{name: "updated_at", colType: "timestamp(6) without time zone"},
+	}
+
+	// Add expCols to table.
+	tbl := Table{name: "users"}
+	for _, v := range expCols {
+		tbl.AddColumn(v) // Run Table.AddColumn()
+	}
+
+	actCols := tbl.columns
+
+	for i, v := range expCols {
+		// Compare column names
+		exp := v.name
+		act := actCols[i].name
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+
+		// Compare column types
+		exp = v.colType
+		act = actCols[i].colType
+		if exp != act {
+			t.Errorf("want %q; got %q", exp, act)
+		}
+	}
+}
