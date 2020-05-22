38eb43ee40fd7e4b97d857d4aa1fe1bdef2abd23
kevinsapp <sapp.kevin@gmail.com>
2020-05-22 20:50:29 +0300

    Added method

--- pkg/sqlt/migration.go
+++ pkg/sqlt/migration.go
@@ -1,5 +1,10 @@
 package sqlt
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Migration ...
 type Migration struct {
 	sql     string
@@ -24,4 +29,28 @@ func (m *Migration) Version() int64 {
 // SetVersion sets version.
 func (m *Migration) SetVersion(ver int64) {
 	m.version = ver
+}
+
+// SetFromFile sets attributes from a migration file.
+func (m *Migration) SetFromFile(filename string) error {
+	fn := filename
+
+	// Extract migration version from filename.
+	fnParts := strings.Split(fn, "_")
+	ver, err := strconv.ParseInt(fnParts[0], 10, 64)
+	if err != nil {
+		return err
+	}
+
+	// Read in SQL content from migration file.
+	sql, err := FileAsString(fn)
+	if err != nil {
+		return err
+	}
+
+	// Set attributes.
+	m.version = ver
+	m.sql = sql
+
+	return err
 }
