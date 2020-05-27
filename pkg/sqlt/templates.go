37e233ca11c22c1000847a918f7fea95732faea6
kevinsapp <sapp.kevin@gmail.com>
2020-05-27 14:28:24 +0300

    Refactoring

--- pkg/sqlt/templates.go
+++ pkg/sqlt/templates.go
@@ -1,5 +1,10 @@
 package sqlt
 
+import (
+	"bytes"
+	"text/template"
+)
+
 // SQL templates for DATABASE operaions
 const (
 	// CreateDBTmpl is a SQL template for creating databases.
@@ -59,3 +64,28 @@ RENAME COLUMN {{.Name}} TO {{.NewName}};
 
 {{end}}`
 )
+
+// ProcessTmpl applies a data structure to a SQL template and returns a string.
+func ProcessTmpl(data interface{}, sqlt string) (string, error) {
+	// Initialize a template.
+	var s string
+	t := template.New("t")
+
+	// Parse the template.
+	t, err := t.Parse(sqlt)
+	if err != nil {
+		return s, err
+	}
+
+	// Apply the data structure to the template and write the result to a buffer.
+	var b bytes.Buffer
+	err = t.Execute(&b, data)
+	if err != nil {
+		return s, err
+	}
+
+	// Get contents of the byte buffer as a string.
+	s = b.String()
+
+	return s, err
+}
