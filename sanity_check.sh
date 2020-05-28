2594b15c7dd1d0ac211750097a6e3985748b5fd8
kevinsapp <sapp.kevin@gmail.com>
2020-05-28 17:45:03 +0300

    Updated sanity check

--- sanity_check.sh
+++ sanity_check.sh
@@ -12,15 +12,16 @@ go install .
 rm -rf migrations
 
 monarch db reset
-monarch g m create table users
-monarch g m add column users givenName:varchar familyName:varchar
+monarch g m create table users givenName:varchar familyName:varchar
+monarch g m add column users email:varchar phone:varchar
 monarch g m rename column users givenName:firstName familyName:lastName
-monarch g m drop column users lastName
+monarch g m drop column users phone
 monarch g m rename table users people
 monarch g m create table cars
 monarch g m add column cars color:varchar
 monarch g m create index cars color
 monarch g m drop table people
+monarch g m drop table cars
 monarch db migrate
 monarch db drop
 
